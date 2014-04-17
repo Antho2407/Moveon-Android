4a7244df82762081c4ab7347410382db0e896a0e
Hugo Martinez <hugo.83300@gmail.com>
2014-04-17 17:28:30 -0400

    Database MoveOn

---
+++ src/com/applicationmoveon/database/Database.java
@@ -0,0 +1,237 @@
+package com.example.movebdd;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.util.Log;
+
+public class Database {
+	
+	
+	public Database() {
+		
+	
+	}
+	
+	public void addUser(String prenom, String nom, String email, String password){
+		//the year data to send
+		ArrayList<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+		nameValuePairs.add(new BasicNameValuePair("prenom",prenom));
+		nameValuePairs.add(new BasicNameValuePair("nom",nom));
+		nameValuePairs.add(new BasicNameValuePair("email",email));
+		nameValuePairs.add(new BasicNameValuePair("password",password));
+		 
+		//http post
+		try{
+		        HttpClient httpclient = new DefaultHttpClient();
+		        HttpPost httppost = new HttpPost("http://martinezhugo.com/moveon/add_user.php");
+		        httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+		        HttpResponse response = httpclient.execute(httppost);
+		}catch(Exception e){
+		        Log.e("log_tag", "Error in http connection "+e.toString());
+		}
+	}
+	
+	public void addEvent(String title, String location, String description, String date_debut, String date_fin, String participants, String id_createur, String date_creation, String state, String urlimage){
+		//the year data to send
+		ArrayList<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+		nameValuePairs.add(new BasicNameValuePair("title",title));
+		nameValuePairs.add(new BasicNameValuePair("location",location));
+		nameValuePairs.add(new BasicNameValuePair("description",description));
+		nameValuePairs.add(new BasicNameValuePair("date_debut",date_debut));
+		nameValuePairs.add(new BasicNameValuePair("date_fin",date_fin));
+		nameValuePairs.add(new BasicNameValuePair("participants",participants));
+		nameValuePairs.add(new BasicNameValuePair("id_createur",id_createur));
+		nameValuePairs.add(new BasicNameValuePair("date_creation",date_creation));
+		nameValuePairs.add(new BasicNameValuePair("state",state));
+		nameValuePairs.add(new BasicNameValuePair("urlimage",urlimage));
+		//http post
+		try{
+		        HttpClient httpclient = new DefaultHttpClient();
+		        HttpPost httppost = new HttpPost("http://martinezhugo.com/moveon/add_event.php");
+		        httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+		        HttpResponse response = httpclient.execute(httppost);
+		}catch(Exception e){
+		        Log.e("log_tag", "Error in http connection "+e.toString());
+		}
+	}
+	
+	public void addParticipants(String id_event, String id_user){
+		//the year data to send
+		ArrayList<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+		nameValuePairs.add(new BasicNameValuePair("id_event",id_event));
+		nameValuePairs.add(new BasicNameValuePair("id_user",id_user));
+		 
+		//http post
+		try{
+		        HttpClient httpclient = new DefaultHttpClient();
+		        HttpPost httppost = new HttpPost("http://martinezhugo.com/moveon/add_participants.php");
+		        httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+		        HttpResponse response = httpclient.execute(httppost);
+		}catch(Exception e){
+		        Log.e("log_tag", "Error in http connection "+e.toString());
+		}
+	}
+	
+	
+	public JSONArray SelectEvent(){
+		JSONArray jArray = null;
+		String result = "";
+		InputStream is = null;
+		//the year data to send
+		ArrayList<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+		nameValuePairs.add(new BasicNameValuePair("year","1980"));
+		 
+		//http post
+		try{
+		        HttpClient httpclient = new DefaultHttpClient();
+		        HttpPost httppost = new HttpPost("http://martinezhugo.com/moveon/select_event.php");
+		        httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+		        HttpResponse response = httpclient.execute(httppost);
+		        HttpEntity entity = response.getEntity();
+		        is = entity.getContent();
+		}catch(Exception e){
+		        Log.e("log_tag", "Error in http connection "+e.toString());
+		}
+		//convert response to string
+		try{
+		        BufferedReader reader = new BufferedReader(new InputStreamReader(is,"iso-8859-1"),8);
+		        StringBuilder sb = new StringBuilder();
+		        String line = null;
+		        while ((line = reader.readLine()) != null) {
+		                sb.append(line + "\n");
+		        }
+		        is.close();
+		 
+		        result=sb.toString();
+		}catch(Exception e){
+		        Log.e("log_tag", "Error converting result "+e.toString());
+		}
+		 
+		//parse json data
+		try{
+		        jArray = new JSONArray(result);
+		}catch(JSONException e){
+		        Log.e("log_tag", "Error parsing data "+e.toString());
+		}
+		return jArray;
+		
+	}
+	
+	public JSONArray SelectUserByMail(String email){
+		JSONArray jArray = null;
+		String result = "";
+		InputStream is = null;
+		//the year data to send
+		ArrayList<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+		nameValuePairs.add(new BasicNameValuePair("email",email));
+		 
+		//http post
+		try{
+		        HttpClient httpclient = new DefaultHttpClient();
+		        HttpPost httppost = new HttpPost("http://martinezhugo.com/moveon/select_user_by_mail.php");
+		        httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+		        HttpResponse response = httpclient.execute(httppost);
+		        HttpEntity entity = response.getEntity();
+		        is = entity.getContent();
+		}catch(Exception e){
+		        Log.e("log_tag", "Error in http connection "+e.toString());
+		}
+		//convert response to string
+		try{
+		        BufferedReader reader = new BufferedReader(new InputStreamReader(is,"iso-8859-1"),8);
+		        StringBuilder sb = new StringBuilder();
+		        String line = null;
+		        while ((line = reader.readLine()) != null) {
+		                sb.append(line + "\n");
+		        }
+		        is.close();
+		 
+		        result=sb.toString();
+		}catch(Exception e){
+		        Log.e("log_tag", "Error converting result "+e.toString());
+		}
+		 
+		//parse json data
+		try{
+		        jArray = new JSONArray(result);
+		
+		
+		}catch(JSONException e){
+		        Log.e("log_tag", "Error parsing data "+e.toString());
+		}
+		return jArray;
+		
+	}
+	
+	
+	public void Fonction(){
+		String toshare = null;
+		String result = "";
+		InputStream is = null;
+		//the year data to send
+		ArrayList<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+		nameValuePairs.add(new BasicNameValuePair("year","1980"));
+		 
+		//http post
+		try{
+		        HttpClient httpclient = new DefaultHttpClient();
+		        HttpPost httppost = new HttpPost("http://martinezhugo.com/script_test.php");
+		        httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+		        HttpResponse response = httpclient.execute(httppost);
+		        HttpEntity entity = response.getEntity();
+		        is = entity.getContent();
+		}catch(Exception e){
+		        Log.e("log_tag", "Error in http connection "+e.toString());
+		}
+		//convert response to string
+		try{
+		        BufferedReader reader = new BufferedReader(new InputStreamReader(is,"iso-8859-1"),8);
+		        StringBuilder sb = new StringBuilder();
+		        String line = null;
+		        while ((line = reader.readLine()) != null) {
+		                sb.append(line + "\n");
+		        }
+		        is.close();
+		 
+		        result=sb.toString();
+		        toshare = result;
+		}catch(Exception e){
+		        Log.e("log_tag", "Error converting result "+e.toString());
+		}
+		 
+		//parse json data
+		try{
+		        JSONArray jArray = new JSONArray(result);
+		        for(int i=0;i<jArray.length();i++){
+		                JSONObject json_data = jArray.getJSONObject(i);
+		                Log.i("log_tag","id: "+json_data.getInt("id")+
+		                        ", name: "+json_data.getString("name")+
+		                        ", sex: "+json_data.getInt("sex")+
+		                        ", birthyear: "+json_data.getInt("birthyear")
+		                );
+		        }
+		
+		}catch(JSONException e){
+		        Log.e("log_tag", "Error parsing data "+e.toString());
+		}
+	}
+		
+}	
+	
+
+
---
+++ src/com/applicationmoveon/database/RequestTask.java
@@ -0,0 +1,72 @@
+package com.example.movebdd;
+import java.util.HashMap;
+
+import org.json.JSONArray;
+
+import android.os.AsyncTask;
+
+public class RequestTask extends AsyncTask<HashMap<String,String>, String, JSONArray> {
+		/*
+		 * Cette méthode s'exécute dans le thread
+		 * de l'interface. C'est le bon endroit
+		 * pour notifier l'usager qu'une tâche
+		 * plus longue commence (par exemple,
+		 * afficher une barre de progression).
+		 * 
+		 */
+		protected void onPreExecute() {
+			// Affiche la barre de progression
+		}
+		
+		/*
+		 * Cette méthode est exécutée dans son
+		 * propre thread. C'est là où le travail le plus
+		 * lourd se passe. On pourra appeler publishProgress
+		 * durant l'exécution de cette méthode pour mettre
+		 * à jour le thread d'interface.
+		 * 
+		 */
+		protected JSONArray doInBackground(HashMap<String,String>... params) {
+			HashMap<String, String> request = params[0];
+			Database web=new Database();
+			if(request.get("Request").equals("SelectEvent")){
+				return web.SelectEvent();
+			}
+			return null;
+			
+		}
+		
+		protected JSONArray AddUser(HashMap<String,String> request){
+			
+			return null;
+			
+		}
+		
+		/*
+		 * Cette méthode est appelée dans le thread
+		 * d'interface lorsque publishProgress est
+		 * appelée dans doInBackground. Les paramètres
+		 * sont passés directement de l'une à l'autre.
+		 * 
+		 */
+		protected void onProgressUpdate(String... s) {
+			// éxécute dans le thread interface, si le thread non-interface
+			// appelle publishProgress à l'intérieur de doInBackground
+		}
+		
+		/*
+		 * Cette méthode s'exécute dans le thread
+		 * d'interface. C'est l'endroit où on
+		 * réagit généralement à la complétion du
+		 * processus en arrière-plan, par exemple
+		 * en mettant à jour l'interface avec
+		 * les données obtenues.
+		 * 
+		 */
+		protected void onPostExecute(String web) {
+			// Cache la barre de progression
+	
+			
+		}
+
+}
