34717b07819788a9cf74543297b1d331f86a5e5b
Hugo Martinez <hugo.83300@gmail.com>
2014-04-17 21:16:18 -0400

    Rajout exectask

---
+++ src/com/applicationmoveon/database/ExecTask.java
@@ -0,0 +1,120 @@
+package com.example.movebdd;
+import java.util.HashMap;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.os.AsyncTask;
+
+public class ExecTask extends AsyncTask<HashMap<String,String>, String, Boolean> {
+		/*
+		 * Cette méthode s'exécute dans le thread
+		 * de l'interface. C'est le bon endroit
+		 * pour notifier l'usager qu'une tâche
+		 * plus longue commence (par exemple,
+		 * afficher une barre de progression).
+		 * 
+		 */
+		protected Database db;
+		
+		public ExecTask(){
+			db =new Database();
+		}
+		protected void onPreExecute() {
+			// Affiche la barre de progression
+		}
+		
+		/*
+		 * Cette méthode est exécutée dans son
+		 * propre thread. C'est là où le travail le plus
+		 * lourd se passe. On pourra appeler publishProgress
+		 * durant l'exécution de cette méthode pour mettre
+		 * à jour le thread d'interface.
+		 * 
+		 */
+		protected Boolean doInBackground(HashMap<String,String>... params) {
+			HashMap<String, String> request = params[0];
+			String query = request.get("Request");
+			
+			if(query.equals("AddEvent")){
+				return AddEvent(request);
+			}else if(query.equals("isValidCombination")){
+				return isValidCombination(request);
+			}
+			
+			return false;
+			
+		}
+		
+		protected Boolean isValidCombination(HashMap<String,String> request){
+			
+			if(request.containsKey("email") && request.containsKey("motdepasse")){
+				JSONArray result = db.SelectUserByMail(request.get("email"));
+				if(result.length() == 1){
+					JSONObject json_data = null;
+					 try {
+						 json_data = result.getJSONObject(0);
+						
+					} catch (JSONException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					try {
+						if(json_data.getString("motdepasse").equals(request.get("motdepasse"))){
+							return true;
+						}
+					} catch (JSONException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+				return false;
+			}
+				
+				
+			return false;
+		}
+		
+		protected Boolean AddEvent(HashMap<String,String> request){
+			
+			if(request.containsKey("title") && request.containsKey("description")&& request.containsKey("location")&& request.containsKey("date_debut")&& request.containsKey("date_fin")
+			&& request.containsKey("participants")&& request.containsKey("id_createur")&& request.containsKey("date_creation")&& request.containsKey("state")
+			&& request.containsKey("urlimage")){
+				db.addEvent(request.get("title"), request.get("location"), request.get("description"), request.get("date_debut"), request.get("date_fin"), 
+				request.get("participants"), request.get("id_createur"), request.get("date_creation"), request.get("state"), request.get("urlimage"));
+				return true;
+			}
+			return false;
+			
+			
+		}
+		
+		/*
+		 * Cette méthode est appelée dans le thread
+		 * d'interface lorsque publishProgress est
+		 * appelée dans doInBackground. Les paramètres
+		 * sont passés directement de l'une à l'autre.
+		 * 
+		 */
+		protected void onProgressUpdate(String... s) {
+			// éxécute dans le thread interface, si le thread non-interface
+			// appelle publishProgress à l'intérieur de doInBackground
+		}
+		
+		/*
+		 * Cette méthode s'exécute dans le thread
+		 * d'interface. C'est l'endroit où on
+		 * réagit généralement à la complétion du
+		 * processus en arrière-plan, par exemple
+		 * en mettant à jour l'interface avec
+		 * les données obtenues.
+		 * 
+		 */
+		protected void onPostExecute(String web) {
+			// Cache la barre de progression
+	
+			
+		}
+
+}
